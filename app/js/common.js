085f64a4099e575775bd68910bdaddd4e58664af
Melnichuk Yaroslav <solmelone@gmail.com>
2016-05-24 14:58:06 +0300

    update htacces

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,10 @@
  $(function() {
 
-	//nav Scroll
-	//Animation lib
-	jQuery.easing['jswing'] = jQuery.easing['swing'];
-
-	jQuery.extend( jQuery.easing,
-	{
-		def: 'easeOutQuad',
-		swing: function (x, t, b, c, d) {
-			//alert(jQuery.easing.default);
-			return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
-		},
-		easeInOutCubic: function (x, t, b, c, d) {
-			if ((t/=d/2) < 1) return c/2*t*t*t + b;
-			return c/2*((t-=2)*t*t + 2) + b;
-		},
-	});
-
 	//Scroll animate
 	$("a[href^='#']").click(function () {
 		var elementClick = $(this).attr("href")
 		var destination = $(elementClick).offset().top;
-		jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 700, "easeInOutCubic");
+		jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 500 );
 		return false;
 	});
 	//SVG Fallback
